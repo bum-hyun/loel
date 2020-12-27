2043684fde0b4111b4d811d670c617dca647e5e6
bum-hyun <ru_bryunak@naver.com>
2020-12-27 21:53:06 +0900

    Feature: Apollo Setting

--- apollo/index.ts
+++ apollo/index.ts
@@ -1,6 +1,7 @@
-import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloClient, createHttpLink, InMemoryCache, ApolloLink } from "@apollo/client";
 import cookie from "js-cookie";
 import { setContext } from "@apollo/client/link/context";
+import { onError } from "@apollo/client/link/error";
 
 const httpLink = createHttpLink({
   uri: "http://localhost:3301/api",
@@ -8,17 +9,31 @@ const httpLink = createHttpLink({
 
 const authLink = setContext((_, { headers }) => {
   const token = cookie.get("accessToken");
-  return {
-    headers: {
-      ...headers,
-      authorization: token,
-    },
-  };
+  if (token) {
+    return {
+      headers: {
+        ...headers,
+        authorization: token,
+      },
+    };
+  }
 });
 
+const errorLink = onError(({ graphQLErrors, networkError, operation, forward }) => {
+  if (graphQLErrors) {
+    console.log(graphQLErrors);
+  }
+  if (networkError) {
+    console.log(`[Network error]: ${networkError}`);
+  }
+});
+
+const link = ApolloLink.from([authLink, errorLink, httpLink]);
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: link,
   cache: new InMemoryCache(),
+  connectToDevTools: true,
 });
 
 export default client;
