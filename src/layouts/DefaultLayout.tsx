f26758deaadbc7e3b77a71e2b629e08d05cc2301
김범현 <bumhyunkim@newin.co.kr>
2021-06-14 17:31:02 +0900

    Fix: 필요없는 코드 제거

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -4,8 +4,6 @@ import styled from "styles/styled";
 import Categories from "../components/Categories";
 import { GetServerSidePropsContext } from "next";
 import CheckList from "components/CheckList";
-import service from "@utils/service";
-import { Button } from "styles";
 
 interface Props extends GetServerSidePropsContext {
   authority: boolean;
@@ -13,11 +11,6 @@ interface Props extends GetServerSidePropsContext {
 }
 
 const DefaultLayout = (Component: (props: Props) => JSX.Element) => (props: Props) => {
-  const makeSitemap = async () => {
-    const result = await service.get("/sitemap/posts.xml");
-    console.log(result);
-  };
-
   return (
     <>
       <Header authority={props.authority} authenticated={props.authenticated} />
@@ -30,7 +23,6 @@ const DefaultLayout = (Component: (props: Props) => JSX.Element) => (props: Prop
           <RightWrap>
             <Categories />
             <CheckList />
-            {props.authority && <Button onClick={makeSitemap}>사이트맵 생성</Button>}
           </RightWrap>
         </Wrap>
       </Container>
